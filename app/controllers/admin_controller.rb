9bad8d801dea9b342c0b3fe8585044020df5355f
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2023-03-21 19:14:12 +0500

    Application: Handled Admin Roles -> Added Devise views -> Removed Registrable from Devise -> Added Admin ability to create new user -> Added Roles to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,26 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   def index
   end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to admin_index_path, flash: { success: 'User Created Successfully' }
+    else
+      flash[:danger] = @admin.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  
+  def admin_params
+    params.require(:admin).permit(:email, :password, :password_confirmation, :role)
+  end
+
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,9 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: { Admin: 0, Cashier: 1 }
+  validates :role, presence: true
 end
