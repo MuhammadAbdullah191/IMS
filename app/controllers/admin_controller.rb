c3f3abfa93fa16a619fda5c243c028ad58612e20
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2023-03-22 17:43:48 +0500

    Application: Added Policies -> Added Pundit Gem -> Added Admin Policy -> Added Admin Dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,21 @@
 class AdminController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_admin, only: [:edit, :update, :destroy]
+
   def index
+    @admins = Admin.all
+    authorize @admins
   end
 
   def new
     @admin = Admin.new
+    authorize @admin
   end
 
   def create
     @admin = Admin.new(admin_params)
+    authorize @admin
+
     if @admin.save
       redirect_to admin_index_path, flash: { success: 'User Created Successfully' }
     else
@@ -18,10 +25,36 @@ class AdminController < ApplicationController
 
   end
 
+  def edit
+  end
+
+  def update
+    if @admin.save
+      redirect_to admin_index_path, flash: { success: 'User Created Successfully' }
+    else
+      flash[:danger] = @admin.errors.full_messages.to_sentence
+      render :new, status: :unprocessable_entity
+    end
+    
+  end
+
+  def destroy
+    @admin.destroy
+    redirect_to admin_index_path, flash: { success: 'Admin Deleted successfully' }
+  end
+
+  def dashboard
+  end
+
   private
   
   def admin_params
     params.require(:admin).permit(:email, :password, :password_confirmation, :role)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+    authorize @admin
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :admin
-  root to: 'admin#index'
+  root to: 'admin#dashboard'
 end
