edbfbed56a01cccd7518e799aa55179d07268256
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2023-05-03 15:07:15 +0500

    Specs: Fixed Spacing issues

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-let(:order) { build(:order) }
+  let(:order) { build(:order) }
 
-describe 'associations' do
-it { is_expected.to have_many(:order_items).dependent(:destroy) }
-end
+  describe 'associations' do
+    it { is_expected.to have_many(:order_items).dependent(:destroy) }
+  end
 
 end
